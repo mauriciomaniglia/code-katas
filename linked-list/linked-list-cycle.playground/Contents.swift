fc1e19480072b9efd69a3a00de1aeb199c9cb545
mauriciomaniglia <manigliamauricio@gmail.com>
2022-09-07 09:44:21 -0300

    Implemented exercise linked list cycle

---
+++ linked-list/linked-list-cycle.playground/Contents.swift
@@ -0,0 +1,86 @@
+import Cocoa
+
+/*
+ 141. Linked List Cycle
+ 
+ Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+ There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+ Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+ Example 1:
+
+ 3 -> 2 -> 0 -> 4
+      ^         |
+      |         |
+      -----------
+
+ Input: head = [3,2,0,-4], pos = 1
+ Output: true
+ Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+ 
+ Example 2:
+
+ 1 -> 2 -
+ ^      |
+ |      |
+ --------
+
+ Input: head = [1,2], pos = 0
+ Output: true
+ Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+ 
+ Example 3:
+ Input: head = [1], pos = -1
+ Output: false
+ Explanation: There is no cycle in the linked list.
+ */
+
+
+//Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
+/*
+ Solution1
+ 
+ floyd's tortoise and hare algorithm
+ 
+ Time: O(n)
+ Space: O(1)
+ */
+
+class Solution1 {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var slow = head
+        var fast = head
+        
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+            
+            if slow === fast { return true }
+        }
+        
+        return false
+    }
+}
+
+let node1 = ListNode(1)
+let node5 = ListNode(5, node1)
+let node4 = ListNode(4, node5)
+let node3 = ListNode(3, node4)
+let node2 = ListNode(2, node3)
+node1.next = node2
+
+
+let solution1 = Solution1()
+solution1.hasCycle(node1)
