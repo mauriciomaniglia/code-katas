05af65be307f65261277df78ef6f750b6a269041
mauriciomaniglia <manigliamauricio@gmail.com>
2024-09-16 21:01:59 -0300

    Create new exercise String Encode/Decode

---
+++ arrays-hashing/string-encode-decode.swift
@@ -0,0 +1,28 @@
+/**
+
+Design an algorithm to encode a list of strings to a single string. 
+The encoded string is then decoded back to the original list of strings.
+
+Please implement encode and decode
+
+Example 1:
+
+Input: ["neet","code","love","you"]
+Output:["neet","code","love","you"]
+
+Example 2:
+
+Input: ["we","say",":","yes"]
+Output: ["we","say",":","yes"]
+
+**/
+
+
+
+func encode(_ list: [String]) -> String {
+
+}
+
+func decode(_ s: String) -> [String] {
+
+}
