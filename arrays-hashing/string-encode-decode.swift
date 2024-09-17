167f2648b23034829f12faa667eb6b2881200d67
mauriciomaniglia <manigliamauricio@gmail.com>
2024-09-17 09:05:37 -0300

    Implement solution for string encode/decode problem

--- arrays-hashing/string-encode-decode.swift
+++ arrays-hashing/string-encode-decode.swift
@@ -20,9 +20,38 @@ Output: ["we","say",":","yes"]
 
 
 func encode(_ list: [String]) -> String {
+    var encodedString = ""
 
+    for s in list {
+        encodedString += "#\(s.count)" + s
+    }
+    
+    return encodedString
 }
 
 func decode(_ s: String) -> [String] {
+    var decodedArray = [String]()
+    let list = Array(s)
+    var d = ""
 
+    for i in 0..<list.count {
+        if list[i] == "#" {
+            d = "#"
+            continue
+        }
+
+        if let number = Int(String(list[i])), d == "#" {
+            let word = s.dropFirst(i+1).prefix(number)
+            decodedArray.append(String(word))
+        }
+    }
+
+    return decodedArray
 }
+
+let input = ["apple","banana","car","blue", "world"]
+let e = encode(input)
+
+
+print(encode(input))
+print(decode(e))
